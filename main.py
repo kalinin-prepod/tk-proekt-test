8bece41595ae9b8a347291ca0cd4e43ab80d8b24
kalinin-prepod <133877570+kalinin-prepod@users.noreply.github.com>
2023-10-13 13:48:53 +0300

    1

---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from settings import *
+
+win = tk.Tk()
+win.title(
+    "Сами пишите а не списывайте у меня а то че у всех одинаковов так же не интересно вообще."
+)
+win.geometry(f"{W}x{H}")
+win.resizable(0, 0)
+
+win.configure(bg = COLOR_1)
+
+button_1 = tk.Button(text="ЖМИ", bg=COLOR_3, fg=COLOR_4)
+button_1.grid(row=1, column=3, sticky="we")
+
+label_1 = tk.Label(text="текст", bg=COLOR_1, fg=COLOR_4)
+label_1.grid(row=0, column=2, sticky="wens")
+
+entry_1 = tk.Entry()
+entry_1.grid(row=1, column=1, sticky="we", columnspan=2)
+
+
+# задаем размеры грида
+for i in range(20):
+    win.grid_columnconfigure(i, minsize=SIZE_W)
+    win.grid_rowconfigure(i, minsize=SIZE_H)
+
+
+win.mainloop()
