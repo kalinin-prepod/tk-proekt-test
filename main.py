d924b0365bb58c251b5b14f9a41896121bf91239
kalinin-prepod <133877570+kalinin-prepod@users.noreply.github.com>
2023-10-13 14:50:56 +0300

    alpha release

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 import tkinter as tk
 from settings import *
+import winsound
+import sys
 
+
+
+def calculate():
+    exp = entry_1.get() # считать что написано в ентри
+    if len(exp) == 0:
+        return
+    try: 
+        res = eval(exp) # посчитать через евал
+    except:
+        res = 'ОШИБКА'
+    entry_1.delete(0, len(exp)) # очистить ентри
+    label_1.configure(text = res) # записать в лейбл результат 
+    
+def click():
+    global count
+    if count > 10:
+        label_1.configure(text = 'РАНГ: ДОБРОСЛАВ')
+    if count > 20:
+        label_1.configure(text = 'РАНГ: БЕЛОГОР')
+    if count > 30:
+        label_1.configure(text = 'РАНГ: ЛЮБОРАД')
+    if count > 40:
+        label_1.configure(text = 'РАНГ: ЯРОМИР')
+    winsound.PlaySound('sound.wav',winsound.SND_ASYNC)
+    count += 1
+    button_2.configure(text= f'УБИТО ЯЩЕРОВ: {count}')
+    
+count = 0
 win = tk.Tk()
 win.title(
     "Сами пишите а не списывайте у меня а то че у всех одинаковов так же не интересно вообще."
@@ -8,20 +38,20 @@ win.title(
 win.geometry(f"{W}x{H}")
 win.resizable(0, 0)
 
+win.configure(bg = COLOR_1)
 
-
-button_1 = tk.Button(text="ЖМИ", bg=COLOR_3, fg=COLOR_4)
+button_1 = tk.Button(text="ЖМИ", bg=COLOR_3, fg=COLOR_4, font=FONT_1, command=lambda: calculate())
 button_1.grid(row=1, column=3, sticky="we")
 
-label_1 = tk.Label(text="текст", bg=COLOR_1, fg=COLOR_4)
+label_1 = tk.Label(text="текст", bg=COLOR_1, fg=COLOR_4, font=FONT_2)
 label_1.grid(row=0, column=2, sticky="wens")
 
-entry_1 = tk.Entry()
+entry_1 = tk.Entry( font=FONT_1, fg=COLOR_2)
 entry_1.grid(row=1, column=1, sticky="we", columnspan=2)
 
-entry_1 = tk.Entry()
-entry_1.grid(row=1, column=1, sticky="we", columnspan=2)
 
+button_2 = tk.Button(text="ЖМИ", bg=COLOR_3, fg=COLOR_4, font=FONT_1, command=lambda: click())
+button_2.grid(row=3, column=3, sticky="we")
 
 # задаем размеры грида
 for i in range(20):
--- settings.py
+++ settings.py
@@ -8,3 +8,7 @@ COLOR_1 = "#43506c"  # темный синий
 COLOR_2 = "#ef4b4c"  # красный
 COLOR_3 = "#3d619b"  # синий
 COLOR_4 = "#e9e9eb"  # белый
+
+
+FONT_1 = ('Comic Sans MS',15)
+FONT_2 = ('Comic Sans MS',18)
